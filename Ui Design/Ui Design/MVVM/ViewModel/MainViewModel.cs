4840aef2fc29430d54c48b060ab6da29c998d908
Apopheniaa <93959230+Apopheniaa@users.noreply.github.com>
2022-05-14 18:31:54 +0800

    Add files via upload

---
+++ Ui Design/Ui Design/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Ui_Design.Core;
+
+namespace Ui_Design.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+
+        }
+    }
+}
