1da850478a3f2c07b4e464c82aca44afc339cf59
Apopheniaa <93959230+Apopheniaa@users.noreply.github.com>
2022-05-20 13:45:13 +0800

    Add files via upload

---
+++ vachart/vachart/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace vachart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(713, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 41);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(776, 397);
+            this.dgvData.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(632, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(614, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ vachart/vachart/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace vachart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            dgvData.DataSource = LoadCSV(textBox1.Text);
+        }
+
+        public List<GenderData> LoadCSV(string csvFile)
+        {
+            var query = from l in File.ReadAllLines(csvFile)
+                        let data = l.Split(',')
+                        select new GenderData
+                        {
+                            DateAndTime = data[0],
+                            Gender = data[1],
+                            FirstDoseDaily = data[2],
+                            FirstDoseCumulative = data[3],
+                            SecondDoseDaily = data[4],
+                            SecondDoseCumulative = data[5],
+                            SingleDoseDailyCumulative = data[6],
+                            ThirdDoseDaily = data[7],
+                            ThirdDoseDailyCumulative = data[8],
+                            AtleastOneDose = data[9],
+                            AtleastOneDoseCumulative = data[10],
+                            FullyVaccinated = data[11],
+                            FullyVaccinatedCumulative = data[12],
+                        };
+            return query.ToList();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            textBox1.Text = dlg.FileName;
+        }
+    }
+
+    public class GenderData
+    {
+        public string DateAndTime { get; set; }
+        public string Gender { get; set; }
+        public string FirstDoseDaily { get; set; }
+        public string FirstDoseCumulative { get; set; }
+        public string SecondDoseDaily { get; set; }
+        public string SecondDoseCumulative { get; set; }
+        public string SingleDoseDaily { get; set; }
+        public string SingleDoseDailyCumulative { get; set; }
+        public string ThirdDoseDaily { get; set; }
+        public string ThirdDoseDailyCumulative { get; set; }
+        public string AtleastOneDose { get; set; }
+        public string AtleastOneDoseCumulative { get; set; }
+        public string FullyVaccinated { get; set; }
+        public string FullyVaccinatedCumulative { get; set; }
+    }
+
+}
